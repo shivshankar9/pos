193a22a69894d6d8346a8884964519df82a8d636
Shiv <69760883+shivshankar9@users.noreply.github.com>
2024-08-20 01:49:45 +0530

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -72,37 +72,30 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "/ui.earth.com",
+    title: "Billing System",
+    des: "A modern billing system that simplifies the billing process for businesses.",
+    img: "/billing.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+    link: "https://makebills.netlify.com",
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "/ui.yoom.com",
+    title: "Employee Management System",
+    des: "An efficient system to manage employees, track performance, and streamline HR processes.",
+    img: "/ems.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+    link: "https://ems.netlify.com",
   },
   {
     id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "/ui.aiimg.com",
-  },
-  {
-    id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "/ui.apple.com",
+    title: "Visitor Management System",
+    des: "A system to manage and track visitors in your premises, ensuring safety and security.",
+    img: "/visitor.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+    link: "/ui.visitor.com",
   },
 ];
+
 
 export const testimonials = [
   {
