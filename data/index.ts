0a4411e0c4d0211c8b25e409549909cc9015c37a
Shiv <69760883+shivshankar9@users.noreply.github.com>
2024-08-23 00:15:54 +0530

    Update index.ts
    experince updates

--- data/index.ts
+++ data/index.ts
@@ -167,33 +167,28 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Full Stack Developer Intern",
+    desc: "Contributed to the development of a web-based platform using React.js, improving interactivity and user experience.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
+
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
-  },
-  {
-    id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-    className: "md:col-span-2", // change to md:col-span-2
+    title: "Freelance App Developer",
+    desc: "Led the development of a mobile app for a client, overseeing the project from initial concept through to deployment on app stores.",
+    className: "md:col-span-2",
     thumbnail: "/exp3.svg",
   },
   {
-    id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    id: 3,
+    title: "Technical Support Specialist- Vyapar App",
+    desc: "Provided technical support and developed user-facing features using modern frontend technologies, enhancing overall functionality.",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
   },
 ];
+
 
 export const socialMedia = [
   {
