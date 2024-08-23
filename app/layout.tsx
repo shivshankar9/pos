998f94c1d0ae1bf296303049eb81d330978867e7
Shiv <69760883+shivshankar9@users.noreply.github.com>
2024-08-24 03:16:44 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Shiv's Portfolio",
+  title: "Beyond Boundaries ",
   description: "Modern & Minimal JS Mastery Portfolio",
 };
 
