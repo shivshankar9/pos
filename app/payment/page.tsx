2716110748cc3d221afbe7d9fccc0b1e3d365066
Shiv <69760883+shivshankar9@users.noreply.github.com>
2024-12-20 14:43:09 +0530

    Update page.tsx

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -24,7 +24,7 @@ const PaymentPage = () => {
   const handlePayment = () => {
     if (typeof window !== "undefined" && (window as any).Razorpay) {
       const options = {
-        key: "rzp_live_hfbBkmLnUZrWdpYour", //rzp_test_6GkVsJKB0s9HHx rzp_live_hfbBkmLnUZrWdpYour Razorpay API key
+        key: "rzp_live_hfbBkmLnUZrWdp", //rzp_test_6GkVsJKB0s9HHx rzp_live_hfbBkmLnUZrWdpYour Razorpay API key
         amount: discountedPrice * 100, // Convert amount to paise
         currency: "INR",
         name: "Finverge.Tech",
