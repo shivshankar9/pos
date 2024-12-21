72a35362d2eb355c65f99367cf2b06b81d33cf9e
shivshankar9 <shivshankarkumar281@gmail.com>
2024-12-21 23:41:49 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -10,34 +10,28 @@ import Experience from "@/components/Experience";
 import Clients from "@/components/Clients";
 import Integrations from "@/components/Integrations";
 import RecentProjects from "@/components/RecentProjects";
+// import FutureUI from "@/components/FutureUI";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
-// import FuturisticBusinessSuite from "@/components/futureui";
-import Head from "next/head"; // Import the Head component from Next.js
+import FuturisticBusinessSuite from "@/components/futureui";
+// Add imported component
 
 const Home = () => {
   return (
-    <>
-      {/* Add favicon link in the Head section */}
-      <Head>
-        <link rel="icon" href="/jsm-logo.png" />{" "}
-        {/* Correct path to the favicon */}
-      </Head>
-      <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-        <div className="max-w-7xl w-full">
-          <FloatingNav navItems={navItems} />
-          <Hero />
-          <Grid />
-          <Notes />
-          <RecentProjects />
-          <Integrations />
-          {/* <FuturisticBusinessSuite /> */}
-          <Clients />
-          <Experience />
-          <Approach />
-          <Footer />
-        </div>
-      </main>
-    </>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <Notes />
+        <RecentProjects />
+        <Integrations />
+        <FuturisticBusinessSuite />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
   );
 };
 
