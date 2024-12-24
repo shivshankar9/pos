3e548ee12facae6dd1c8172553328630009b0c63
shivshankar9 <shivshankarkumar281@gmail.com>
2024-12-25 03:25:02 +0530

    update

--- app/finance/layout.js
+++ app/finance/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    // <ClerkProvider>
       <html lang="en">
         <head>
           <link rel="icon" href="/logo-sm.png" sizes="any" />
@@ -30,6 +30,6 @@ export default function RootLayout({ children }) {
           </footer>
         </body>
       </html>
-    </ClerkProvider>
+    // </ClerkProvider>
   );
 }
