e5bc304dd2816f72c56de3e3c0918bd8b491f8e4
shivshankar9 <shivshankarkumar281@gmail.com>
2024-12-26 19:19:50 +0530

    Revert "Reapply "update""
    This reverts commit d69e08607d0f4b725c694ca79fd812f1958a3d1c.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,40 @@
-// app/layout.tsx
-import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
 import "./globals.css";
+import { ThemeProvider } from "./provider";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Beyond Boundaries ",
+  description: "Modern & Minimal JS Mastery Portfolio",
+};
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <head>
-          <link rel="icon" href="/favicon.ico" />
-        </head>
-        <body>{children}</body>
-      </html>
+        <ClerkProvider>
+
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="finvergelogo.png" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
     </ClerkProvider>
   );
 }
