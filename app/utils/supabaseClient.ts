2339d007817d03c9a595d2885b37c664545c1288
shivshankar9 <shivshankarkumar281@gmail.com>
2024-12-31 00:36:17 +0530

    Create supabaseClient.ts

---
+++ app/utils/supabaseClient.ts
@@ -0,0 +1,14 @@
+// utils/supabaseClient.ts
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+
+console.log("Supabase URL:", supabaseUrl);
+console.log("Supabase Key:", supabaseKey);
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase URL and Key are required");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
